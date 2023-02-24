b9a8053c2611ed210e15a9db94868c93c7b8fa05
Hastwell <turner8193@gmail.com>
2023-02-23 20:21:42 -0800

    Correct spelling on Rainier

--- Omukade.Cheyenne/RainierKnownTypesBinder.cs
+++ Omukade.Cheyenne/RainierKnownTypesBinder.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace Omukade.Cheyenne
 {
-    public class RainerKnownTypesBinder : ISerializationBinder
+    public class RainierKnownTypesBinder : ISerializationBinder
     {
-        public static readonly RainerKnownTypesBinder INSTANCE;
+        public static readonly RainierKnownTypesBinder INSTANCE;
 
         static Dictionary<string, Type> RECOGNIZED_TYPES;
-        static RainerKnownTypesBinder()
+        static RainierKnownTypesBinder()
         {
             Type[] TYPES_FROM_TARGETED_ASSEMBLIES = new Type[] { typeof(MatchLogic.PlayerSelectionInfo), typeof(Platform.Sdk.Models.Matchmaking.BeginMatchmaking) };
             RECOGNIZED_TYPES = TYPES_FROM_TARGETED_ASSEMBLIES.Select(t => t.Assembly)
@@ -20,7 +20,7 @@ namespace Omukade.Cheyenne
                 .Where(t => t.FullName != "MatchLogic.AbstractBlob") // Prevent gadget attacks
                 .ToDictionary(TypeToLookupName);
 
-            INSTANCE = new RainerKnownTypesBinder();
+            INSTANCE = new RainierKnownTypesBinder();
         }
 
         private static string TypeToLookupName(Type t) => $"{t.Assembly.GetName().Name}+{t.FullName}";
