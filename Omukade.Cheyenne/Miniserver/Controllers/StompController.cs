a84480aa4f6430197ca28cc132b6748aca6a359e
Hastwell <turner8193@gmail.com>
2023-02-23 22:11:31 -0800

    Add license header to all source files

--- Omukade.Cheyenne/Miniserver/Controllers/StompController.cs
+++ Omukade.Cheyenne/Miniserver/Controllers/StompController.cs
@@ -1,8 +1,22 @@
-﻿using FlatBuffers;
-#if !NOIMEX
-using ImexV.Core;
-using ImexV.Core.Messages.InternalMessages.RainierShim;
-#endif
+﻿/*************************************************************************
+* Omukade Cheyenne - A PTCGL "Rainier" Standalone Server
+* (c) 2022 Hastwell/Electrosheep Networks 
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+* 
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+using FlatBuffers;
 using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
